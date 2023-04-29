705d964774a4cba230b2219aade9ff4e0ef1d272
Piotr Zaniewski <piotr@upbound.io>
2023-04-29 13:49:58 +0200

    Test stubs

--- internal/aicmd/aicmd_test.go
+++ internal/aicmd/aicmd_test.go
@@ -2,6 +2,7 @@ package aicmd
 
 import (
 	"fmt"
+	"io"
 	"strings"
 	"testing"
 
@@ -27,7 +28,107 @@ func TestShouldExecuteCommand(t *testing.T) {
 	result := shouldExecuteCommand(config, input)
 	fmt.Printf("Result: %v\n", result) // Add this line to print the result
 
-	if result {
-		t.Error("Expected result to be false, but it was true")
+	if result == CmdDoNothing {
+		t.Error("Expected command to be executed")
+	}
+}
+
+func TestDefaultExecutor_Execute(t *testing.T) {
+	type args struct {
+		command string
+	}
+	tests := []struct {
+		name    string
+		e       *DefaultExecutor
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := &DefaultExecutor{}
+			if err := e.Execute(tt.args.command); (err != nil) != tt.wantErr {
+				t.Errorf("DefaultExecutor.Execute() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_shouldExecuteCommand(t *testing.T) {
+	type args struct {
+		config *config.Config
+		reader io.Reader
+	}
+	tests := []struct {
+		name string
+		args args
+		want CommandDecision
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := shouldExecuteCommand(tt.args.config, tt.args.reader); got != tt.want {
+				t.Errorf("shouldExecuteCommand() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_copyCommandToClipboard(t *testing.T) {
+	type args struct {
+		command string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := copyCommandToClipboard(tt.args.command); (err != nil) != tt.wantErr {
+				t.Errorf("copyCommandToClipboard() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_appendCommandToHistory(t *testing.T) {
+	type args struct {
+		command string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			appendCommandToHistory(tt.args.command)
+		})
+	}
+}
+
+func TestExecute(t *testing.T) {
+	type args struct {
+		prompt_file string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Execute(tt.args.prompt_file); (err != nil) != tt.wantErr {
+				t.Errorf("Execute() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
 	}
 }
